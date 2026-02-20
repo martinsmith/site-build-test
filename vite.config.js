6158316eaa5e02164b2b7a09faac865ea2c11e24
Martin Smith <martagnan@gmail.com>
2026-02-20 11:51:56 +0000

    Added sector template

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         'new-lift-installations': resolve(__dirname, 'new-lift-installations.html'),
+        'commercial-lifts': resolve(__dirname, 'commercial-lifts.html'),
       },
     },
   },
